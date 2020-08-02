3c7f3d9a188528f3fb62a46aed0cba6ce922650f
Michael Silva <msilva@aggienetwork.com>
2020-08-02 11:59:03 -0500

    moved service worker to public directory, changed files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,28 @@
 const FILES_TO_CACHE = [
-    "./server.js",
-    "./models/transaction.js",
-    "./routes/api",
-    "./public/index.html",
-    "./public/manifest.json",
-    "./public/css/styles",
-    "./public/js/idb.js",
-    "./public/js/index.js",
-    
+    "../models/transaction.js",
+    "./index.html",
+    "./manifest.json",
+    "./css/styles",
+    "./js/idb.js",
+    "./js/index.js",
+    "/",
+    "./icons/icon-72x72",
+    "./icons/icon-96x96",
+    "./icons/icon-128x128",
+    "./icons/icon-144x144",
+    "./icons/icon-152x152",
+    "./icons/icon-192x192",
+    "./icons/icon-384x384",
+    "./icons/icon-512x512",
 ];
 const APP_PREFIX = 'Trust-Funder-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 //use self because service workers run before window exists
-self.addEventListener('install', function (e) {
+self.addEventListener('install', function (event) {
     //wait until work is complete before terminating the service worker.
-    e.waitUntil(
+    event.waitUntil(
         //find specific cache by name, then add every file in the files to cache array
         caches.open(CACHE_NAME).then(function (cache) {
             console.log('installing cache : ' + CACHE_NAME)
@@ -26,8 +32,8 @@ self.addEventListener('install', function (e) {
 })
 
 //return array of all cache names, call it keyList
-self.addEventListener('activate', function(e) {
-    e.waitUntil(
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
       caches.keys().then(function(keyList) {
         let cacheKeeplist = keyList.filter(function(key) {
           return key.indexOf(APP_PREFIX);
@@ -49,16 +55,16 @@ self.addEventListener('activate', function(e) {
 // listen for fetch event, log URL of requested resource
 // respondWith to intercept fetch request with a check to see if the request is stored or not.
 // If stored, this will deliver the resource directly from cache, otherwise, retrieve normally.
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request: ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
+self.addEventListener('fetch', function (event) {
+    console.log('fetch request: ' + event.request.url)
+    event.respondWith(
+        caches.match(event.request).then(function (request) {
             if (request) {
-                console.log('responding with cache: ' + e.request.url)
+                console.log('responding with cache: ' + event.request.url)
                 return request
             } else {
-                console.log('file is not cached, fetching: ' + e.request.url)
-                return fetch(e.request)
+                console.log('file is not cached, fetching: ' + event.request.url)
+                return fetch(event.request)
             }
         })
     )
